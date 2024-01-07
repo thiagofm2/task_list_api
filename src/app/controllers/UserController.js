8f24d835951dcf3c43733d9f03f6a14aa8bcfee7
thiagofm2 <thiago.fm012@gmail.com>
2024-01-06 21:59:43 -0300

    creating update user route

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -22,6 +22,32 @@ class UserController {
     }
 
     async update(req, res) {
+        const { email, oldPassword } = req.body;
+
+        const user = await User.findByPk(req.userId);
+
+        if (email !== user.email) {
+            const userExists = await User.findOne({
+                where: { email },
+            });
+
+            if (userExists) {
+                return res.status(400).json({ error: 'Usuário já existe' });
+            }
+        }
+
+        if (oldPassword && !(await user.checkPassword(oldPassword))) {
+            return res.status(401).json({ error: 'Senha incorreta.' });
+        }
+
+        const { id, name } = await user.update(req.body);
+
+        return res.json({
+            id,
+            name,
+            email,
+        });
+
         return res.json();
     }
 }
